860e09294070c4822bdee3027e09805df0e0a90b
Pavel <pavel@MacBook-Air-DNS.local>
2015-01-18 14:40:18 +0300

    Initial Commit

---
+++ AskMeWhy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AskMeWhy
+//
+//  Created by Pavel on 18.01.15.
+//  Copyright (c) 2015 Zitech Mobile LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AskMeWhy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AskMeWhy
+//
+//  Created by Pavel on 18.01.15.
+//  Copyright (c) 2015 Zitech Mobile LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
