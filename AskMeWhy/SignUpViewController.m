1ed6ecb879bc64351b5312150e1f30477c2be70c
Pavel <pavel@MacBook-Air.local>
2015-02-08 20:39:15 +0300

    Textfield features on Sign Up screen.

--- AskMeWhy/SignUpViewController.m
+++ AskMeWhy/SignUpViewController.m
@@ -15,6 +15,10 @@
 
 @implementation SignUpViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
 
 -(void)signUpButtonPressed:(UIButton *)sender {
     
@@ -27,13 +31,65 @@
     user[@"username"] = [self.usernameTextField text];
     user[@"password"] = [self.passwordTextField text];
     [user saveInBackground];
+    
+    // Do segue
+    [self performSegueWithIdentifier: @"showMain" sender: self];
 }
 
+// Dismiss keyboard
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    
     // done button was pressed - dismiss keyboard
     [textField resignFirstResponder];
     return YES;
 }
+// ==================================================================================================================
+
+// Clear and turn back default text in text fields
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    [textField setText:@""];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    
+    if (![textField hasText]) {
+        if ([[textField restorationIdentifier]  isEqual: @"user"]) {
+            [textField setText: @"Username"];
+        }
+        if ([[textField restorationIdentifier]  isEqual: @"pass"]) {
+            [textField setText: @"Password"];
+        }
+    }
+}
+
+// ==================================================================================================================
+
+// Move up the screen
+//Declare a delegate, assign your textField to the delegate and then include these methods
+
+-(BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
+    return YES;
+}
+
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidHide:) name:UIKeyboardDidHideNotification object:nil];
+    
+    [self.view endEditing:YES];
+    return YES;
+}
+
+- (void)keyboardDidShow:(NSNotification *)notification
+{
+    // Assign new frame to your view
+    [self.view setFrame:CGRectMake(0,-110,320,460)]; //here taken -20 for example i.e. your view will be scrolled to -20. change its value according to your requirement.
+}
+
+-(void)keyboardDidHide:(NSNotification *)notification
+{
+    [self.view setFrame:CGRectMake(0,0,320,460)];
+}
+
+// ==================================================================================================================
 
 @end
